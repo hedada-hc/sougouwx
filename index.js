721222f2a69612e705919d87b59f0f328997c8ad
hezone <183844707@qq.com>
2017-02-25 01:56:11 +0800

    搜狗主页验证码算法破解

--- index.js
+++ index.js
@@ -76,3 +76,16 @@ ipc.on("code",(error,success) => {
 	code.loadURL(success)
 	console.log(success)
 })
+
+/*
+{"code": 0,"msg": "解封成功，正在为您跳转来源地址...", "id": "7F2AB15025206F6F7AC489A426C3E2AD"}  id 等于 SNUID     还要 SUV 取 SUIR 字段时间 加上123123456 
+
+
+Cookie: ABTEST=7|1487957620|v1; IPLOC=CN; SUID=590C97752930990A0000000058B06E74; PHPSESSID=p2dlttfoproqnsb6rod1h6nff5; SUIR=1487957620; SUV=1487957620123123456; SNUID=E5B72BC9BBBEF2F19EEE171BBC8BB094; seccodeRight=success; successCount=1|Fri, 24 Feb 2017 17:36:51 GMT; refresh=1
+cookie: IPLOC=CN; SUIR=1487957714; SUV=1487957714123123456; ABTEST=0|1487957714|v1; SUID=590C97752930990A0000000058B06ED2; PHPSESSID=fb2icja3db9qpe2flh9ku89oj3
+
+; seccodeRight=success; successCount=1|Fri, 24 Feb 2017 17:51:38 GMT; refresh=1
+
+http://pb.sogou.com/pv.gif?uigs_productid=webapp&type=antispider&subtype=0_seccodeInputSuccess&domain=weixin&suv=000D178C75970C5958B062A6529B9220&snuid=6431AB483C397575610815813DF8D090&t=1487954187273
+http://pb.sogou.com/pv.gif?uigs_productid=webapp&type=antispider&subtype=close_refresh&domain=weixin&suv=000D178C75970C5958B062A6529B9220&snuid=&t=1487954588950
+*/
